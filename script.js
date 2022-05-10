dab7b97e9c5d3c8e462b84ec9c29a5c9154683b0
AbraaoAbe <79542473+AbraaoAbe@users.noreply.github.com>
2022-05-09 21:13:20 -0300

    jogo do dinossauro criado e funcionando

---
+++ script.js
@@ -0,0 +1,73 @@
+const dino = document.querySelector('.dino');
+const bg = document.querySelector('.background');
+let position = 0;
+
+let taPulando = false;
+
+function getKey(event) {
+    if (event.key === " ") {
+        console.log("jump");
+        if (!taPulando) {
+            jump();
+        }
+    }
+}
+
+function jump() {
+    taPulando = true;
+
+    let upInterval = setInterval(() => {
+        if (position >= 200) {
+            clearInterval(upInterval);
+            let downInterval = setInterval(() => {
+                position -= 20;
+                dino.style.bottom = position + 'px';
+
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                    taPulando = false;
+                }
+            }, 30);
+        } else {
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+
+
+
+    }, 20);
+}
+
+function criaCactos() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    bg.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+
+
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            bg.removeChild(cactus);
+
+            //colisao
+        } else if (cactusPosition > 0 && cactusPosition <= 60 && position <= 60) {
+            //gameOver
+
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class ="gameover">Fim de Jogo</h1>';
+
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20)
+
+    setTimeout(criaCactos, randomTime);
+}
+document.addEventListener('keyup', getKey)
+criaCactos();
